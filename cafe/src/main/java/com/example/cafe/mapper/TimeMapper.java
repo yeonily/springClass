6a9a5edd5eadba218e5b32fb0ede79f8ccaecc9e
yeonily <wluds9791@gmail.com>
2022-12-01 17:55:55 +0900

    패키지 정리

--- cafe/src/main/java/com/example/cafe/mapper/TimeMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.cafe.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TimeMapper {
-    public String getTime();
-}
--- delivery/src/main/java/com/example/delivery/mapper/TimeMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.delivery.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TimeMapper {
-    public String getTime();
-}
